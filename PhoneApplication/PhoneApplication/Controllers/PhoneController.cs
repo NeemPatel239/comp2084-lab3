c652fd57a99b52d1f5daff4b8003b54b7bbb3cfc
Neem Patel <200429225@student.georgianc.on.ca>
2020-01-23 21:43:25 -0500

    completed lab3

---
+++ PhoneApplication/PhoneApplication/Controllers/PhoneController.cs
@@ -0,0 +1,27 @@
+﻿using PhoneApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PhoneApplication.Controllers
+{
+    public class PhoneController : Controller
+    {
+        // GET: Phone
+        public ActionResult Index()
+        {
+            var PhoneList = new List<Phone>
+            {
+                new Phone { PhoneName = "Samsung note 10+" , PhoneId = 0, Manufacturer ="US", MSRP=0, ScreenSize= 6, DateRelease = new DateTime(2000,09,06)},
+                new Phone {PhoneName = "Iphone 11 pro", , PhoneId = 0, Manufacturer ="US", MSRP=0, ScreenSize= 6, DateRelease = new DateTime(2019,10,06)},
+                new Phone {PhoneName = "OnePlus 7", , PhoneId = 0, Manufacturer ="US", MSRP=0, ScreenSize= 6, DateRelease = new DateTime(2019,04,06)},
+                new Phone {PhoneName = "Gionee", , PhoneId = 0, Manufacturer ="US", MSRP=0, ScreenSize= 6, DateRelease = new DateTime(2000,09,06)},
+                new Phone {PhoneName = "Samsung galaxy 8", , PhoneId = 0, Manufacturer ="US", MSRP=0, ScreenSize= 6, DateRelease = new DateTime(2000,09,06)}
+            };
+
+            return View(PhoneList);
+        }
+    }
+}
---
+++ PhoneApplication/PhoneApplication/Models/Phone.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PhoneApplication.Models
+{
+    public class Phone
+    {
+        public Phone()
+        {
+            PhoneId = 0;
+            PhoneName = "";
+            Manufacturer = "";
+            MSRP = 0;
+            ScreenSize = 0;
+            DateRelease = new DateTime();
+           
+      
+        }
+        public int PhoneId { get; set; }
+        public string PhoneName { get; set; }
+        public string Manufacturer { get; set; }
+        public Double MSRP { get; set; }
+        public Double ScreenSize { get; set; }
+        public DateTime DateRelease { get; set; }
+
+    }
+}
